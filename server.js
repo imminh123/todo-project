2ee4f48565e290359e0f45b17759293c160ec0b8
Minh Nguyen <minhco12@gmail.com>
2018-12-18 23:42:10 +0700

    'sfd

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
   });
 const TodoRouter = require('./routers/todoRouter');
